1ff1595ad9c489788cf6707bca4d9d7439e9c956
dungeonmaster161 <uttam.shikhar16@gmail.com>
2024-05-21 22:07:23 +0530

    Home page categories images are added

--- frontend/src/customer/pages/homePage/HomePage.jsx
+++ frontend/src/customer/pages/homePage/HomePage.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Navigation from '../../components/navigation/Navigation'
 import MainCrousel from '../../components/homeCarousel/MainCarousel'
+import HomeCategories from '../../components/homeCategories/HomeCategories'
 
 export default function HomePage() {
   return (
     <>
-     {/* <MainCrousel /> */}
+     <HomeCategories />
     </>
   )
 }
