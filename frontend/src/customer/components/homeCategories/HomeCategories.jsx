9393538d14efe47b8a18a39077677a9b6e666374
dungeonmaster161 <uttam.shikhar16@gmail.com>
2024-05-21 22:07:41 +0530

    Home page categories images are added

--- frontend/src/customer/components/homeCategories/HomeCategories.jsx
+++ frontend/src/customer/components/homeCategories/HomeCategories.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function HomeCategories() {
   return (
-    <div className='flex flex-row'>
+    <div className='flex'>
       <div className=' basis-11/12 '><img src='https://raw.githubusercontent.com/dungeonmaster161/sneaker-store/master/frontend/public/bannerOne.webp' /></div>
       <div className='basis-1/2 flex flex-col'>
         <div className=''> <img src='https://raw.githubusercontent.com/dungeonmaster161/sneaker-store/master/frontend/public/bannerTwo.webp' /> </div>
