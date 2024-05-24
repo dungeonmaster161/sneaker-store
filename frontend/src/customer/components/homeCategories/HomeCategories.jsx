817d6be66d4fcc6ecfdfd005e34597a4f51574f5
dungeonmaster161 <uttam.shikhar16@gmail.com>
2024-05-24 22:46:11 +0530

    tailwind is added

--- frontend/src/customer/components/homeCategories/HomeCategories.jsx
+++ frontend/src/customer/components/homeCategories/HomeCategories.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 export default function HomeCategories() {
   return (
+    //Explanation about is added
     <div className='flex'>
       <div className=' basis-11/12 '><img src='https://raw.githubusercontent.com/dungeonmaster161/sneaker-store/master/frontend/public/bannerOne.webp' /></div>
       <div className='basis-1/2 flex flex-col'>
